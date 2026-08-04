fc35ec88a9e5ce114a9e5b77190da6740a034802
jcsdiaz <camilo.salas@cenigaa.org>
2026-08-03 20:18:37 -0500

    03 feat : avance indicadores demograficos

---
+++ react-javascript/develop/src/app/dashboard/indicadores-demograficos/charts/GenderChart.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+export default function GenderChart() {
+
+return (
+
+    <div className="bg-red-500 text-white p-10 rounded-xl">
+
+        HOLA SOY GENDER CHART
+
+    </div>
+
+);
+
+}
